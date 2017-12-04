5af89856c1ca91e450019353efb81a7bf9d7495f
Pete Bachant <pete@windesco.com>
2017-12-04 13:08:27 -0500

    Add pure python rainflow counting module

---
+++ src/AeroelasticSE/rainflow/pyrainflow.py
@@ -0,0 +1,96 @@
+from numpy import fabs as fabs
+import numpy as np
+
+def rainflow(array_ext, flm=0, l_ult=1e16, uc_mult=0.5):
+    """ Rainflow counting of a signal's turning points with Goodman correction
+
+        Args:
+            array_ext (numpy.ndarray): array of turning points
+
+        Keyword Args:
+            flm (float): fixed-load mean [opt, default=0]
+            l_ult (float): ultimate load [opt, default=1e16]
+            uc_mult (float): partial-load scaling [opt, default=0.5]
+
+        Returns:
+            array_out (numpy.ndarray): (5 x n_cycle) array of rainflow values:
+                                        1) load range
+                                        2) range mean
+                                        3) Goodman-adjusted range
+                                        4) cycle count
+                                        5) Goodman-adjusted range with flm = 0
+
+
+    From https://gist.github.com/jennirinker/688a917ccb7a9c14e78f
+
+    """
+
+    flmargin = l_ult - fabs(flm)            # fixed load margin
+    tot_num = array_ext.size                # total size of input array
+    array_out = np.zeros((5, tot_num-1))    # initialize output array
+
+    pr = 0                                  # index of input array
+    po = 0                                  # index of output array
+    j = -1                                  # index of temporary array "a"
+    a  = np.empty(array_ext.shape)          # temporary array for algorithm
+
+    # loop through each turning point stored in input array
+    for i in range(tot_num):
+
+        j += 1                  # increment "a" counter
+        a[j] = array_ext[pr]    # put turning point into temporary array
+        pr += 1                 # increment input array pointer
+
+        while ((j >= 2) & (fabs( a[j-1] - a[j-2] ) <= \
+                fabs( a[j] - a[j-1]) ) ):
+            lrange = fabs( a[j-1] - a[j-2] )
+
+            # partial range
+            if j == 2:
+                mean      = ( a[0] + a[1] ) / 2.
+                adj_range = lrange * flmargin / ( l_ult - fabs(mean) )
+                adj_zero_mean_range = lrange * l_ult / ( l_ult - fabs(mean) )
+                a[0]=a[1]
+                a[1]=a[2]
+                j=1
+                if (lrange > 0):
+                    array_out[0,po] = lrange
+                    array_out[1,po] = mean
+                    array_out[2,po] = adj_range
+                    array_out[3,po] = uc_mult
+                    array_out[4,po] = adj_zero_mean_range
+                    po += 1
+
+            # full range
+            else:
+                mean      = ( a[j-1] + a[j-2] ) / 2.
+                adj_range = lrange * flmargin / ( l_ult - fabs(mean) )
+                adj_zero_mean_range = lrange * l_ult / ( l_ult - fabs(mean) )
+                a[j-2]=a[j]
+                j=j-2
+                if (lrange > 0):
+                    array_out[0,po] = lrange
+                    array_out[1,po] = mean
+                    array_out[2,po] = adj_range
+                    array_out[3,po] = 1.00
+                    array_out[4,po] = adj_zero_mean_range
+                    po += 1
+
+    # partial range
+    for i in range(j):
+        lrange    = fabs( a[i] - a[i+1] );
+        mean      = ( a[i] + a[i+1] ) / 2.
+        adj_range = lrange * flmargin / ( l_ult - fabs(mean) )
+        adj_zero_mean_range = lrange * l_ult / ( l_ult - fabs(mean) )
+        if (lrange > 0):
+            array_out[0,po] = lrange
+            array_out[1,po] = mean
+            array_out[2,po] = adj_range
+            array_out[3,po] = uc_mult
+            array_out[4,po] = adj_zero_mean_range
+            po += 1
+
+    # get rid of unused entries
+    array_out = array_out[:,:po]
+
+    return array_out
